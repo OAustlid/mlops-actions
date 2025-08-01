8fb77ac5e05311adf05dee5391accec0f6303661
OAustlid <oaustlid@gmail.com>
2025-08-01 10:48:12 +0200

    list-action

--- subaction/subapp.py
+++ subaction/subapp.py
@@ -1,46 +1,42 @@
 import typer
+from azure.core.credentials import AccessToken
 from azure.identity import DefaultAzureCredential, AzureCliCredential,ChainedTokenCredential
 from azure.ai.ml import MLClient
 import os
 
-app = typer.Typer()
+class Credential:
+    def __init__(self,access_token):
+        self._access_token = access_token
+    def get_token(self, *scopes: str, claims: str | None = None, tenant_id: str | None = None, enable_cae: bool = False, **kwargs: any)-> AccessToken:
+        return self._access_token
 
-@app.command()
-def help():
-    print(f"""
-    Provide a subscription_id, a resource group name and a workspace_name.
-    Then this app will print out the environments it finds beneath it.
-    """)
-
-@app.command()
-def getenvlist(token:str,subscription_id:str,resource_group_name:str,workspace_name:str):
-    print(f"Environments in /SUBSCRIPTIONS/{subscription_id}/RESOURCEGROUPS/{resource_group_name}/PROVIDERS/Microsoft.MachineLearningServices/WORKSPACES/{workspace_name} :")
-    print(f"Length of token: {len(token)}")
-    
-    #access_token = os.environ.get("AZURE_ACCESS_TOKEN")
-
-    #if (token is None) or (token ==""):
-    #    credential = ChainedTokenCredential(
-    #        AzureCliCredential(process_timeout=10),
-    #        DefaultAzureCredential(process_timeout=10)
-    #    )
-    #else:
-    credential = DefaultAzureCredential(managed_identity_client_id=token)
-
+def get_client(token:str,expires_on:int,subscription_id:str,resource_group_name:str,workspace_name:str)->MLClient:
+    credential = Credential(AccessToken(token=token,expires_on=expires_on))
     client = MLClient(
         credential=credential,
         subscription_id=subscription_id,
         resource_group_name=resource_group_name,
         workspace_name=workspace_name
     )
+    return MLClient
+
+app = typer.Typer()
+
+@app.command()
+def environments(token:str,expires_on:int,subscription_id:str,resource_group_name:str,workspace_name:str):
+    print(f"Environments in /SUBSCRIPTIONS/{subscription_id}/RESOURCEGROUPS/{resource_group_name}/PROVIDERS/Microsoft.MachineLearningServices/WORKSPACES/{workspace_name} :")
+    client = get_client(token,expires_on,subscription_id,resource_group_name,workspace_name)
     e_list = list(client.environments.list())
     for e in e_list:
         print(f"Environment: {e.name}, {e.latest_version}")
 
 @app.command()
-def listenvvars():
-    for k, v in os.environ.items():
-        print(f"{k}={v}")
+def components(token:str,expires_on:int,subscription_id:str,resource_group_name:str,workspace_name:str):
+    print(f"Components in /SUBSCRIPTIONS/{subscription_id}/RESOURCEGROUPS/{resource_group_name}/PROVIDERS/Microsoft.MachineLearningServices/WORKSPACES/{workspace_name} :")
+    client = get_client(token,expires_on,subscription_id,resource_group_name,workspace_name)
+    c_list = list(client.components.list())
+    for c in c_list:
+        print(f"Component: {c.name}, {c.latest_version}")
 
 if __name__ == "__main__":
     app()
